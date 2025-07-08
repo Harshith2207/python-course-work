504134e4b54168b1b5159eb200f3ff4851d766f0
Harshith2207 <2207harshith@gmail.com>
2025-07-08 12:18:52 +0530

    python ms

--- Class Works/7.2.DictionaryExecution.py
+++ Class Works/7.2.DictionaryExecution.py
@@ -1,1 +1,39 @@
-7.2 Dictionary Execution
+#7.2 Dictionary Execution
+
+#Syntax of Dictionary
+''' dict_name={key1: value1, key2: value2, key3: value3} '''
+
+student = {
+"name": "Harshith",
+"id": 123,
+"course": "Cyber Security"
+}
+print(student) #{'name': 'Harshith', 'id': 123, 'course': 'Cyber Security'}
+
+#Accessing values
+
+print(student["name"]) #Output: Harshith
+print(student.get("id")) # Output: 123
+print(student.get("city", "Not Available")) # Output: Not Available
+
+#Adding & Updating Items
+
+student["id"] = 456 # Updating existing key
+student["city"] = "New York" # Adding a new key-value pair
+print(student) #{'name': 'Harshith', 'id': 456, 'course': 'Cyber Security', 'city': 'New York'}
+
+#Removing Items
+
+id = student.pop("id")
+print(id) # Output: 456
+print(student) #{'name': 'Harshith', 'course': 'Cyber Security', 'city': 'New York'}
+
+del student["course"]
+print(student)  #{'name': 'Harshith', 'city': 'New York'}
+
+student.popitem()
+print(student)  #{'name': 'Harshith'}
+
+student.clear()
+print(student) #Output: {}
+
